88a0c155c992c24de0b261c4ba3cc45837453a8a
@kjh1113_0 <2104210776@qq.com>
2024-03-28 18:58:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MainLayout from './views/MainLayout.vue';
+</script>
+
+<template>
+  <div >
+    <router-view />
+    <MainLayout />
+  </div>
+</template>
+
+<style>
+.router-link-active {
+    text-decoration: none;
+    color: black
+  }
+  a{
+    text-decoration: none;
+    color: grey;
+  }
+
+</style>
+
+
